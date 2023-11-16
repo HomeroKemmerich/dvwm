649d154b781df05caac5c0e8d96544d1757b35b2
Homero Kemmerich <146676@upf.br>
2023-11-15 23:38:09 -0300

    Spring Boot

--- Contents/spotter_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "spotter": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
--- Contents/spotter_app/lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'app/app_widget.dart';
-
-main() {
-  runApp(const AppWidget());
-}
